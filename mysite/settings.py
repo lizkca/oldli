14ea17b0b945114aed6ce3f6a0e9fa45edb7d82b
lizkca <lienxiong@outlook.com>
2025-04-23 18:27:38 +0800

    local use sqlite3 ,render use postgreSQL

--- mysite/settings.py
+++ mysite/settings.py
@@ -82,15 +82,13 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-     'default': dj_database_url.config(
-        # Replace this value with your local database's connection string.
-        default='postgresql://postgres:postgres@localhost:5432/mysite',
+    'default': dj_database_url.config(
+        default='sqlite:///db.sqlite3',  # 本地默认使用 SQLite
         conn_max_age=600
-    )
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
+    ) if 'RENDER' in os.environ else {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 # Password validation
