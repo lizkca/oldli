c79161b71e201b36da51929dd7850d8349720a40
lizkca <lienxiong@outlook.com>
2025-05-05 17:16:33 +0800

    add the number of user

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 from .models import BlogPost
 from .forms import BlogPostForm
 
@@ -9,14 +10,21 @@ def blog_list(request):
 
 @login_required
 def blog_write(request):
-    if request.method == 'POST':
-        form = BlogPostForm(request.POST)
-        if form.is_valid():
-            blog_post = form.save(commit=False)
-            blog_post.author = request.user
-            blog_post.save()
-            return redirect('blog:list')
-    else:
-        form = BlogPostForm()
-    
-    return render(request, 'blog/write.html', {'form': form})
+    try:
+        if request.method == 'POST':
+            form = BlogPostForm(request.POST)
+            if form.is_valid():
+                blog_post = form.save(commit=False)
+                blog_post.author = request.user
+                blog_post.save()
+                messages.success(request, '博客发布成功！')
+                return redirect('blog:list')
+            else:
+                messages.error(request, '表单验证失败，请检查输入。')
+        else:
+            form = BlogPostForm()
+        
+        return render(request, 'blog/write.html', {'form': form})
+    except Exception as e:
+        messages.error(request, f'发生错误：{str(e)}')
+        return redirect('blog:list')
--- homepage/views.py
+++ homepage/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+from django.contrib.auth import get_user_model
 
 def index(request):
-    return render(request, 'homepage/index.html')
+    User = get_user_model()
+    user_count = User.objects.count()
+    return render(request, 'homepage/index.html', {
+        'user_count': user_count
+    })
